43e3392ae7e15d1b783e2a752d511418d0e6c4a2
Raphael Fernandes <raphael.fernandes.eng@gmail.com>
2024-12-10 16:20:08 +0100

    set up

---
+++ app/src/main/java/com/example/automacorp/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.automacorp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.automacorp.ui.theme.AutomacorpTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AutomacorpTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text(
+            text = "I learn to create a new app",
+            modifier = modifier
+        )
+        Button(onClick = {}) {
+            Text(
+                text = "My first button",
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AutomacorpTheme {
+        Greeting("Android")
+    }
+}
